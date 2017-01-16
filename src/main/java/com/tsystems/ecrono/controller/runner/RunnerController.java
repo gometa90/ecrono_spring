6f17a06767e987809f17290aa5b91d6db07e9299
PC36\userPC46 <gometa90@gmail.com>
2017-01-16 18:06:43 +0100

    feat: añado las entitys para probar con el runnerRepository implementado por interfaz jpa

--- src/main/java/com/tsystems/ecrono/controller/runner/RunnerController.java
+++ src/main/java/com/tsystems/ecrono/controller/runner/RunnerController.java
@@ -2,35 +2,49 @@ package com.tsystems.ecrono.controller.runner;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tsystems.ecrono.dto.Runner;
+import com.tsystems.ecrono.domain.RunnerEntity;
+import com.tsystems.ecrono.repository.RunnerRepository;
 
 @RestController
 @RequestMapping("runners")
 public class RunnerController {
 
+    private final RunnerRepository runnerRepository;
+
+    @Autowired
+    public RunnerController(RunnerRepository runnerRepository) {
+	super();
+	this.runnerRepository = runnerRepository;
+    }
+
     @RequestMapping(method = RequestMethod.GET)
-    public List<Runner> getRunners() {
-	throw new IllegalArgumentException("Not implemented yet");
+    public List<RunnerEntity> getRunners() {
+	return runnerRepository.findAll();
     }
 
     @RequestMapping(value = "{id:\\d+}", method = RequestMethod.GET)
-    public Runner getRunnersById(@PathVariable("id") Long raceId) {
+    public RunnerEntity getRunnersById(@PathVariable("id") Long runnerId) {
 
-	throw new IllegalArgumentException("Not implemented yet");
+	return runnerRepository.findOne(runnerId);
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public Runner createRunners() {
-	throw new IllegalArgumentException("Not implemented yet");
+    public RunnerEntity createRunners(@RequestParam(name = "fullname", required = true) String fullName) {
+	RunnerEntity runnerEntity = new RunnerEntity();
+	runnerEntity.setFullName(fullName);
+	runnerRepository.save(runnerEntity);
+	return runnerEntity;
     }
 
     @RequestMapping(value = "{id:\\d+}", method = RequestMethod.PUT)
-    public Runner editRunners() {
+    public RunnerEntity editRunners() {
 	throw new IllegalArgumentException("Not implemented yet");
     }
 
---
+++ src/main/java/com/tsystems/ecrono/domain/component/RaceType.java
@@ -0,0 +1,7 @@
+package com.tsystems.ecrono.domain.component;
+
+public enum RaceType {
+
+    SPRINT, MARATHON, HAL_MARATHON, IRON_MAN;
+
+}
