a1c173df48693c6b7ee01f2b30e0d05516c28e05
PC36\userPC46 <gometa90@gmail.com>
2017-01-16 16:40:43 +0100

    feat: controller y dto para runners y para dorsal

---
+++ src/main/java/dorsalcontroller/DorsalController.java
@@ -0,0 +1,24 @@
+package dorsalcontroller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import dto.Dorsal;
+
+@RestController
+@RequestMapping("dorsals")
+public class DorsalController {
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Dorsal createDorsal(@PathVariable("id") Long raceId, @PathVariable("id") Long runnerId) {
+	throw new IllegalArgumentException("Not implemented yet");
+    }
+
+    @RequestMapping(value = "{id:\\d+}", method = RequestMethod.DELETE)
+    public void deleteDorsal() {
+	throw new IllegalArgumentException("Not implemented yet");
+    }
+
+}
