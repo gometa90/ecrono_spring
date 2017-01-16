c1357a9b7b4b66be3b36cf150ea35066ca0096e9
PC36\userPC46 <gometa90@gmail.com>
2017-01-16 16:40:01 +0100

    feat_añadir la clase del dto y el controlador

---
+++ src/main/java/com/tsystems/ecrono/controllers/racecontrollers/RaceController.java
@@ -0,0 +1,41 @@
+package com.tsystems.ecrono.controllers.racecontrollers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import dto.Race;
+
+@RestController
+@RequestMapping("races")
+public class RaceController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Race> getRaces() {
+	throw new IllegalArgumentException("Not implemented yet");
+    }
+
+    @RequestMapping(value = "{id:\\d+}", method = RequestMethod.GET)
+    public Race getRacesById(@PathVariable("id") Long raceId) {
+
+	throw new IllegalArgumentException("Not implemented yet");
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public Race createRace() {
+	throw new IllegalArgumentException("Not implemented yet");
+    }
+
+    @RequestMapping(value = "{id:\\d+}", method = RequestMethod.PUT)
+    public Race editRace() {
+	throw new IllegalArgumentException("Not implemented yet");
+    }
+
+    @RequestMapping(value = "{id:\\d+}", method = RequestMethod.DELETE)
+    public void deleteRace() {
+	throw new IllegalArgumentException("Not implemented yet");
+    }
+}
